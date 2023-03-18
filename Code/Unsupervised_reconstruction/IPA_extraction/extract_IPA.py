a291631ed3fc120ae0602aa791e94986c9389af5
PilorumNatum <thomas.horrut33@gmail.com>
2023-03-18 19:37:39 +0100

    🔤added IPA phonemes

---
+++ Code/Unsupervised_reconstruction/IPA_extraction/extract_IPA.py
@@ -0,0 +1,21 @@
+from json import load
+import csv
+
+#Creating a csv file from the json one
+def generateCsv_toScrap():
+    with open('IPA_extraction\\scraped_IPA.json', 'r', encoding="utf-8") as file:
+        IPA:list[dict[str, str]] = load(file)
+        for i in range(len(IPA)):
+            for key in IPA[i]:
+                value = IPA[i][key]
+                if value.endswith('\t') or value.endswith('\n'):
+                    value = value[:-1]
+                IPA[i][key]=value
+        with open('IPA_characters.csv','w', encoding='UTF32') as csvFile:
+            writer = csv.DictWriter(csvFile, fieldnames=IPA[0].keys())
+            writer.writeheader()
+            writer.writerows(IPA)
+
+with open('IPA_extraction\\scraped_IPA.json', 'r', encoding="utf-8") as file:
+    IPA:list[dict[str, str]] = load(file)
+    print(len(IPA))

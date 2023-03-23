3014f3749f26f3156a29d1515e7d086f6b0d1437
Thomas Horrut <79534557+PilorumNatum@users.noreply.github.com>
2023-03-23 16:12:36 +0100

    new extract IPA

--- Code/Unsupervised_reconstruction/IPA_extraction/extract_IPA.py
+++ Code/Unsupervised_reconstruction/IPA_extraction/extract_IPA.py
@@ -1,9 +1,11 @@
 from json import load
 import csv
+from os import chdir, path
+chdir(path.dirname(__file__))
 
 #Creating a csv file from the json one
 def generateCsv_toScrap():
-    with open('IPA_extraction\\scraped_IPA.json', 'r', encoding="utf-8") as file:
+    with open('scraped_IPA.json', 'r', encoding="utf-8") as file:
         IPA:list[dict[str, str]] = load(file)
         for i in range(len(IPA)):
             for key in IPA[i]:
@@ -17,8 +19,29 @@ def generateCsv_toScrap():
             writer.writerows(IPA)
 
 def IPA_charactersNumber():
-    with open('IPA_extraction\\scraped_IPA.json', 'r', encoding="utf-8") as file:
+    with open('scraped_IPA.json', 'r', encoding="utf-8") as file:
         IPA:list[dict[str, str]] = load(file)
         print(len(IPA))
 
-generateCsv_toScrap()
+def extractIPAFromDatabase():
+    IPA_lst = set()
+    with open('./../romance-ipa.txt', 'r', encoding='utf-8') as file:
+        SEP = '\t'
+        lines = file.readlines()
+        attrs = lines[0].split(SEP)
+        for i in range(1, len(lines)):
+            line = lines[i]
+            for word in line.split(SEP):
+                for char in word:
+                    IPA_lst.add(char)
+    IPA_lst.remove('\n')
+    with open('IPA_characters.txt', 'w', encoding='utf-8') as file2write:
+        content = ""
+        for elt in IPA_lst:
+            content += elt + ', '
+        file2write.write(content[:-2])
+    return IPA_lst
+
+print(extractIPAFromDatabase())
+
+#generateCsv_toScrap()

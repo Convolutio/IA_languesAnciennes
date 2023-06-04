7d1840e641d0e635d2ad51947bb423fc33611244
PilorumNatum <thomas.horrut33@gmail.com>
2023-06-04 16:42:40 +0200

    🐛🧪

--- Code/Unsupervised_reconstruction/Tests/timeToSample.py
+++ Code/Unsupervised_reconstruction/Tests/timeToSample.py
@@ -6,14 +6,12 @@ def generateProposalsOverDataset():
     cognates = getCognatesSet()
     reconstructions = getIteration(4)
     numberOfCognatePairs = len(cognates['french'])
+    languages = ('spanish', 'portuguese', 'italian', 'romanian', 'french')
     for i in range(numberOfCognatePairs):
         x = reconstructions[i]
-        for language in ('spanish', 'portuguese', 'italian', 'romanian', 'french'):
-            y = cognates[language][i]
-            editDistance = computeMinEditDistanceMatrix(x, y)[len(x), len(y)]
-            if editDistance < 16:
-                print(f'/{x}/ to /{y}/ -{editDistance}- (<{language}>)\nIteration {str(i)}/{numberOfCognatePairs}', end='\r')
-                p = computeProposals(x,cognates[language][i])
+        Y = [cognates[language][i] for language in languages]
+        print(f'/{x}/ to (${Y}) \nIteration {str(i)}/{numberOfCognatePairs}', end='\r')
+        p = computeProposals(x,Y)
 
 def editDistancesInDataset():
     cognates = getCognatesSet()

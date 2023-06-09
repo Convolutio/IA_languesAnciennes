e41ab6c1cdc2d4665b94a70464eef5fcb2348c93
PilorumNatum <thomas.horrut33@gmail.com>
2023-06-09 16:07:55 +0200

    🏗️generation with no string list

--- Code/Unsupervised_reconstruction/Types/articleModels.py
+++ Code/Unsupervised_reconstruction/Types/articleModels.py
@@ -2,13 +2,15 @@ from enum import Enum
 from typing import Union, Literal
 import numpy.typing as npt
 import numpy as np
+from torch import Tensor
 
 ModernLanguages = Literal['french', 'spanish', 'italian', 'portuguese', 'romanian']
 class op(Enum):
     sub='sub'
     ins='ins'
 
-CognatesSet = dict[ModernLanguages, list[str]]
+CognatesSet_str = dict[ModernLanguages, list[str]]
+CognatesSet_oneHotIdxs = dict[ModernLanguages, list[Tensor]]
 
 Form = str
 FormsSet = list[str] # a list of forms, 
--- Code/Unsupervised_reconstruction/lm/PriorLM.py
+++ Code/Unsupervised_reconstruction/lm/PriorLM.py
@@ -1,9 +1,9 @@
-from torch import Tensor
+from Types.models import InferenceData
 from numpy import ndarray
 
 class PriorLM:
     def __init__(self):
         pass
 
-    def inference(self, reconstructions:Tensor)->ndarray:
+    def inference(self, reconstructions:InferenceData)->ndarray:
         pass

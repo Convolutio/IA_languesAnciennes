26efe62c24433e5571a816a82a03f4061a67af35
HORRUT Thomas <thomas.horrut@bordeaux-inp.fr>
2024-01-18 17:48:58 +0100

    🐛 type error + edited some notebooks

--- Code/Unsupervised_reconstruction/data/reconstruction_datasets.py
+++ Code/Unsupervised_reconstruction/data/reconstruction_datasets.py
@@ -9,17 +9,17 @@ from Source.utils import pad2d_sequence
 from models.types import ModernLanguages, Operations, InferenceData_Samples, InferenceData_Cognates, PADDING_TOKEN
 
 
-__TensorCognates = dict[ModernLanguages, Tensor]
+TensorCognates = dict[ModernLanguages, Tensor]
 
 # ---------------Maximisation Datasets---------------------
 
-__RawSample = str
-__RawCognates = dict[ModernLanguages, str]
+RawSample = type(str)
+RawCognates = dict[ModernLanguages, str]
 # TODO: Rework data structure : duplication dict modernlanguages
-__CachedTargetProbs = dict[ModernLanguages, dict[Operations, Tensor]]
+CachedTargetProbs = dict[ModernLanguages, dict[Operations, Tensor]]
 
 
-def __training__collate_fn(batch: list[tuple[__RawSample, __RawCognates, __CachedTargetProbs]]):
+def __training__collate_fn(batch: list[tuple[RawSample, RawCognates, CachedTargetProbs]]):
     """
     Collates the input and target data in the batch.
     """
@@ -29,22 +29,21 @@ def __training__collate_fn(batch: list[tuple[__RawSample, __RawCognates, __Cache
     firstElement = computeInferenceData_Samples(
         wordsToOneHots([t[0] for t in batch]))
 
-    cognates_batch: __TensorCognates = {
+    cognates_batch: TensorCognates = {
         lang: wordsToOneHots([t[1][lang] for t in batch]) for lang in languages}
     secondElement = computeInferenceData_Cognates(cognates_batch)
 
     maxSourceLength = firstElement[2] - 1
     maxCognateLength = {lang: secondElement[lang][2] for lang in languages}
-    lastElement: __CachedTargetProbs = {lang: {op: pad2d_sequence([t[2][lang][op] for t in batch], 0)[
+    lastElement: CachedTargetProbs = {lang: {op: pad2d_sequence([t[2][lang][op] for t in batch], 0)[
         :maxSourceLength, :maxCognateLength[lang]].squeeze(3) for op in operations} for lang in languages}
 
     return (firstElement, secondElement, lastElement)
 
 
-class __TrainingDataset(Dataset[tuple[__RawSample, __RawCognates, __CachedTargetProbs]]):
-    def __init__(self, raw_samples: list[__RawSample], raw_cognates: list[__RawCognates], target_probs: list[__CachedTargetProbs]) -> None:
-        assert (len(raw_samples) == len(raw_cognates) == len(target_probs)
-                ), "The lists to be zipped must have the same length."
+class __TrainingDataset(Dataset[tuple[RawSample, RawCognates, CachedTargetProbs]]):
+    def __init__(self, raw_samples: list[RawSample], raw_cognates: list[RawCognates], target_probs: list[CachedTargetProbs]) -> None:
+        assert(len(raw_samples) == len(raw_cognates) == len(target_probs)), "The lists to be zipped must have the same length."
 
         self.training_load = list(zip(raw_samples, raw_cognates, target_probs))
         self.length = len(target_probs)
@@ -52,15 +51,15 @@ class __TrainingDataset(Dataset[tuple[__RawSample, __RawCognates, __CachedTarget
     def __len__(self) -> int:
         return self.length
 
-    def __getitem__(self, index: int) -> tuple[__RawSample, __RawCognates, __CachedTargetProbs]:
+    def __getitem__(self, index: int) -> tuple[RawSample, RawCognates, CachedTargetProbs]:
         return self.training_load[index]
 
 
-def trainingDataLoader(raw_samples: list[__RawSample],
-                       raw_cognates: list[__RawCognates],
-                       target_probs: list[__CachedTargetProbs],
+def trainingDataLoader(raw_samples: list[RawSample],
+                       raw_cognates: list[RawCognates],
+                       target_probs: list[CachedTargetProbs],
                        mini_batch_size: int, num_workers: int = 1) \
-        -> DataLoader[tuple[InferenceData_Samples, dict[ModernLanguages, InferenceData_Cognates], __CachedTargetProbs]]:
+        -> DataLoader[tuple[InferenceData_Samples, dict[ModernLanguages, InferenceData_Cognates], CachedTargetProbs]]:
 
     training_dataset = __TrainingDataset(raw_samples,
                                          raw_cognates,
@@ -77,9 +76,9 @@ def trainingDataLoader(raw_samples: list[__RawSample],
 
 # -------------Sampling Datasets---------------------
 
-__AbsoluteCoords = tuple[int, int]
-__RawMiniBatch = tuple[tuple[tuple[Tensor, ...], list[__TensorCognates]],
-                            __AbsoluteCoords]
+AbsoluteCoords = tuple[int, int]
+RawMiniBatch = tuple[tuple[tuple[Tensor, ...], list[TensorCognates]],
+                            AbsoluteCoords]
 """
 - tuple of c ByteTensors of shape (b, |x|)
 - list of c dicts with the c cognates group
@@ -87,12 +86,12 @@ __RawMiniBatch = tuple[tuple[tuple[Tensor, ...], list[__TensorCognates]],
 """
 
 
-class __SamplingDataset(IterableDataset[__RawMiniBatch]):
+class __SamplingDataset(IterableDataset[RawMiniBatch]):
     """
     Returns an iterable of tuple([list of c tuple(b samples tensor, associated cognates)], coords in global batch)
     """
 
-    def __init__(self, proposals: list[Tensor], cognates: list[__TensorCognates],
+    def __init__(self, proposals: list[Tensor], cognates: list[TensorCognates],
                  samples_number_per_batch: int, cognates_batch_index: int) -> None:
         assert (len(proposals) == len(cognates)
                 ), "the two lists must have the same length to match together"
@@ -102,7 +101,7 @@ class __SamplingDataset(IterableDataset[__RawMiniBatch]):
         self.batch_size = samples_number_per_batch
         self.cognates_batch_index = cognates_batch_index
 
-    def __iter__(self) -> Generator[__RawMiniBatch, Any, None]:
+    def __iter__(self) -> Generator[RawMiniBatch, Any, None]:
         worker_info = get_worker_info()
         worker_id, num_workers = 0, 1
 
@@ -117,13 +116,13 @@ class __SamplingDataset(IterableDataset[__RawMiniBatch]):
             samples_batch_index += num_workers
 
 
-def __sampling__collate_fn(batch: __RawMiniBatch) \
-        -> tuple[tuple[InferenceData_Samples, dict[ModernLanguages, InferenceData_Cognates]], __AbsoluteCoords]:
+def __sampling__collate_fn(batch: RawMiniBatch) \
+        -> tuple[tuple[InferenceData_Samples, dict[ModernLanguages, InferenceData_Cognates]], AbsoluteCoords]:
 
     collated_samples = computeInferenceData_Samples(pad_sequence(
         sequences=[t.T for t in batch[0][0]], padding_value=vocabulary[PADDING_TOKEN]))
 
-    collated_cognates: __TensorCognates = {}
+    collated_cognates: TensorCognates = {}
     c = len(batch[0][0])
     for language in batch[0][1][0]:
         collated_cognates[language] = pad_sequence(
@@ -132,7 +131,7 @@ def __sampling__collate_fn(batch: __RawMiniBatch) \
     return ((collated_samples, computeInferenceData_Cognates(collated_cognates)), batch[1])
 
 
-def generateSamplingDataset(proposals: list[Tensor], cognates: list[__TensorCognates], batch_shape: __AbsoluteCoords):
+def generateSamplingDataset(proposals: list[Tensor], cognates: list[TensorCognates], batch_shape: AbsoluteCoords):
     """
     Arguments:
      - batch_shape: (c, b)
@@ -146,6 +145,8 @@ def generateSamplingDataset(proposals: list[Tensor], cognates: list[__TensorCogn
     return ChainDataset(datasets)
 
 
-def samplingDataLoader(proposals: list[Tensor], cognates: list[__TensorCognates], batch_shape: __AbsoluteCoords, num_workers: int = 0):
+def samplingDataLoader(proposals: list[Tensor], cognates: list[TensorCognates], batch_shape: AbsoluteCoords, num_workers: int = 0):
+    # it is expected that this result is true for each element in the `proposals` and `cognates` list
+    assert(len(proposals) == len(cognates) <= batch_shape[0] and proposals[0].shape[0] <= batch_shape[1]), "The given batch shape is incorrect"
     #TODO: Rework the data structure the 'collate_fn' expected a list of elements but instead gave a tuple where the lists are contained inside.
     return DataLoader(dataset=generateSamplingDataset(proposals, cognates, batch_shape), batch_size=None, collate_fn=__sampling__collate_fn, num_workers=num_workers)
